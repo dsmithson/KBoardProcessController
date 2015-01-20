076d87cca0e43df02bc4b18807adb028f3d23e0d
Derek Smithson <derek.smithson@knightware.net>
2015-01-20 06:51:00 -0700

    Initial Commit
    First commit of app and kboard library

---
+++ src/CSharpConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Knightware.IO.Devices;
+using System.Threading;
+
+namespace Knightware
+{
+    class Program
+    {
+        static KBoardDevice KBoard;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Up...");
+            KBoard = new KBoardDevice();
+            if(!KBoard.Startup(KBoard_KeyPressed, KBoard_BendChanged))
+            {
+                Console.WriteLine("Failed to startup");
+                return;
+            }
+            
+            Console.WriteLine("Started.  Press any key on the KBoard, and press return to exit");
+            Console.WriteLine();
+
+            //TEST FOR gc ISSUES
+            ThreadPool.QueueUserWorkItem((state) =>
+                {
+                    while (true)
+                    {
+                        Thread.Sleep(1000);
+                        GC.Collect();
+                    }
+                });
+
+            Console.ReadLine();
+            Console.WriteLine("Shutting down...");
+
+            KBoard.Shutdown();
+            KBoard = null;
+        }
+
+        static void KBoard_KeyPressed(object sender, KBoardKeyPressEventArgs e)
+        {
+            //Note:  sender is null - haven't figure out how to get the reference into the even caller in c++/cli land
+            //var KBoard = (KBoardDevice)sender;
+
+            Console.WriteLine("Key ID: 0x{0:X2} ({0} Decimal)\t{1}", e.KeyId, e.IsPressed ? "Pressed" : "Released");
+
+            if (e.IsPressed)
+                KBoard.SetKey(e.KeyId, 0xff);
+            else
+                KBoard.SetKey(e.KeyId, 0x00);
+        }
+
+        static void KBoard_BendChanged(object sender, KBoardBendChangedEventArgs e)
+        {
+            Console.WriteLine("Bend changed to {0:X2} ({0} Decimal)", e.BendValue);
+        }
+    }
+}
---
+++ src/CppConsoleApp/CppConsoleApp.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+
+using namespace std;
+
+void OnButtonPressed(KBoard* sender, unsigned char keyId, bool isPressed, void* userState)
+{
+	cout << (isPressed ? ": pressed" : ": released") << endl;
+
+	if (isPressed)
+		sender->SetKey(keyId, 0xFF);
+	else
+		sender->SetKey(keyId, 0x00);
+}
+
+void OnBendChanged(KBoard* sender, unsigned char bendValue, void* userState)
+{
+	cout << "Bend Changed to 0x";
+	printf("%.2X", bendValue);
+	cout << endl;
+}
+
+int main(int argc, char* argv[])
+{
+	auto kBoard = make_shared<KBoard>();
+	if (!kBoard->Startup(KBoard::KBoardButtonPressCallback(OnButtonPressed), KBoard::KBoardBendChangedCallback(OnBendChanged), nullptr))
+	{
+		cout << "Error starting up." << endl;
+		return -1;
+	}
+
+	cout << "Started.  Press any key on the KBoard, and press return to exit" << endl;
+	getchar();
+
+	cout << "Shutting down..." << endl;
+	kBoard->Shutdown();
+	return 0;
+}
